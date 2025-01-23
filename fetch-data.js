5540073b94cada5cd166ddaecdb9a09017d0c590
Akram <mazakram04@gmail.com>
2025-01-23 08:00:42 -0500

    add fetch

---
+++ fetch-data.js
@@ -0,0 +1,23 @@
+async function fetchUserData() {
+
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById("api-data");
+
+    try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = "";
+
+        const userlist = document.createElement("ul");
+        users.forEach(user => {
+            const list = document.createElement("li");
+            list.textContent = user.name;
+            userlist.appendChild(list)
+        });
+        dataContainer.appendChild(userlist)
+    } catch (error) {
+        dataContainer.innerHTML = "failed to load user data"
+    }
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData())
