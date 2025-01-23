ac9aaec929a9497e1686b526d6c37e38f02f600f
Akram <mazakram04@gmail.com>
2025-01-23 08:02:26 -0500

    add modifications

--- fetch-data.js
+++ fetch-data.js
@@ -8,15 +8,15 @@ async function fetchUserData() {
         const users = await response.json();
         dataContainer.innerHTML = "";
 
-        const userlist = document.createElement("ul");
+        const userList = document.createElement("ul");
         users.forEach(user => {
             const list = document.createElement("li");
             list.textContent = user.name;
-            userlist.appendChild(list)
+            userList.appendChild(list)
         });
-        dataContainer.appendChild(userlist)
+        dataContainer.appendChild(userList)
     } catch (error) {
-        dataContainer.innerHTML = "failed to load user data"
+        dataContainer.innerHTML = "failed to load user data."
     }
 }
 
